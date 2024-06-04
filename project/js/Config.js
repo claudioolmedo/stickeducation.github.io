25c93ced62ba89132881f8e6474af80af5a89fe8
claudioolmedo <109242493+claudioolmedo@users.noreply.github.com>
2024-06-04 18:48:25 -0400

    Revert "add to save in the firebase the config."
    This reverts commit 9207ea6e79fe01e309b7a8b4b6c3e3fdf080972b.

--- project/js/Config.js
+++ project/js/Config.js
@@ -42,28 +42,6 @@ function Config() {
 
 	}
 
-	if (window.localStorage[name]) {
-		const localData = JSON.parse(window.localStorage[name]);
-		for (const key in localData) {
-			storage[key] = localData[key];
-		}
-	}
-
-	firebase.auth().onAuthStateChanged(function(user) {
-		if (user) {
-			const db = firebase.database();
-			const ref = db.ref('configurations/' + user.uid);
-			ref.once('value').then(function(snapshot) {
-				const firebaseData = snapshot.val();
-				if (firebaseData) {
-					for (const key in firebaseData) {
-						storage[key] = firebaseData[key];
-					}
-				}
-			});
-		}
-	});
-
 	return {
 
 		getKey: function ( key ) {
@@ -72,20 +50,18 @@ function Config() {
 
 		},
 
-		setKey: function () {
-			for (let i = 0, l = arguments.length; i < l; i += 2) {
-				storage[arguments[i]] = arguments[i + 1];
+		setKey: function () { // key, value, key, value ...
+
+			for ( let i = 0, l = arguments.length; i < l; i += 2 ) {
+
+				storage[ arguments[ i ] ] = arguments[ i + 1 ];
+
 			}
 
-			// Salvar no localStorage
-			window.localStorage[name] = JSON.stringify(storage);
+			window.localStorage[ name ] = JSON.stringify( storage );
 
-			// Salvar no Firebase
-			const db = firebase.database();
-			const ref = db.ref('configurations/' + firebase.auth().currentUser.uid);
-			ref.set(storage)
-				.then(() => console.log('Saved config to Firebase.'))
-				.catch(error => console.error('Error saving config to Firebase:', error));
+			console.log( '[' + /\d\d\:\d\d\:\d\d/.exec( new Date() )[ 0 ] + ']', 'Saved config to LocalStorage.' );
+
 		},
 
 		clear: function () {

1781df1dea9be74ec10b5cfabb5cc72be0189274
claudioolmedo <109242493+claudioolmedo@users.noreply.github.com>
2024-06-12 11:35:48 -0400

    Revert "impor signals for push button"
    This reverts commit ab817392cae1ba3c23b4aa38085f7fe1bebb32a6.

--- project/js/Storage.js
+++ project/js/Storage.js
@@ -1,6 +1,5 @@
 import { firebaseAuth, firebaseDB, onAuthStateChanged, saveData } from './config/firebase.js';
 import { ref, get } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-database.js";
-import signals from 'path/to/signals.min.js'; // Import the signals library
 
 function Storage() {
 

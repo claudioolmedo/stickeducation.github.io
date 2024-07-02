9379c41a81c8eb60fc1b0f88cdfa52a435e61957
claudioolmedo <109242493+claudioolmedo@users.noreply.github.com>
2024-07-02 16:21:26 -0400

    change format for save in firebase

--- p/js/StorageOnline.js
+++ p/js/StorageOnline.js
@@ -2,8 +2,18 @@ import { saveData } from './config/firebase.js';
 export function sendDataToStorageOnline(data, projectId) {
     console.log('Data received by StorageOnline:', data);
     console.log('Project ID received: in StorageOnline', projectId); // Log the received projectId
+
+    // Transform the data to match the Firebase format
+    const transformedData = {
+        state: {
+            project: data[0].project,
+            camera: data[0].camera,
+            scene: data[0].scene
+        }
+    };
+
     const path = `projects/${projectId}/state`; // Define the path where you want to save the data
-    saveData(path, data)
+    saveData(path, transformedData)
         .then(() => {
             console.log('Data successfully saved to Firebase.');
         })

63ca7671b6a7990e1384d231bd8d4782cb115cc9
claudioolmedo <109242493+claudioolmedo@users.noreply.github.com>
2024-07-02 15:26:18 -0400

    projectId in StorageONline

--- p/js/StorageOnline.js
+++ p/js/StorageOnline.js
@@ -2,7 +2,16 @@ import { saveData } from './config/firebase.js';
 
 export function sendDataToStorageOnline(data) {
     console.log('Data received by StorageOnline:', data);
-    const path = `projects/${data.projectId}/state`; // Define the path where you want to save the data
+    
+    // Use the global projectId
+    const projectId = window.projectId;
+    
+    if (!projectId) {
+        console.error('Project ID is not defined.');
+        return;
+    }
+
+    const path = `projects/${projectId}/state`; // Define the path where you want to save the data
     saveData(path, data)
         .then(() => {
             console.log('Data successfully saved to Firebase.');

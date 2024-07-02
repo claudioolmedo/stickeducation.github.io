22d0f3e71042aa5393e6eb8901b28b98de7eb2b7
claudioolmedo <109242493+claudioolmedo@users.noreply.github.com>
2024-07-02 15:14:03 -0400

    Revert "Add check for projectId"
    This reverts commit 937318a8f896f067cda734817952a4b522842fd8.

--- p/js/StorageOnline.js
+++ p/js/StorageOnline.js
@@ -1,11 +1,6 @@
 import { saveData } from './config/firebase.js';
 
 export function sendDataToStorageOnline(data) {
-    if (!data.projectId) {
-        console.error('Project ID is undefined. Cannot save data to Firebase.');
-        return;
-    }
-
     console.log('Data received by StorageOnline:', data);
     const path = `projects/${data.projectId}/state`; // Define the path where you want to save the data
     saveData(path, data)

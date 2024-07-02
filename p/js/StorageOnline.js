f7412cfda536bfc9b5bcfd7b84abf5a274a350b9
claudioolmedo <109242493+claudioolmedo@users.noreply.github.com>
2024-07-02 14:58:56 -0400

    save projeto to firebase using firebase.js

--- p/js/StorageOnline.js
+++ p/js/StorageOnline.js
@@ -1,3 +1,13 @@
+import { saveData } from './config/firebase.js';
+
 export function sendDataToStorageOnline(data) {
     console.log('Data received by StorageOnline:', data);
+    const path = `projects/${data.projectId}/state`; // Define the path where you want to save the data
+    saveData(path, data)
+        .then(() => {
+            console.log('Data successfully saved to Firebase.');
+        })
+        .catch((error) => {
+            console.error('Error saving data to Firebase:', error);
+        });
 }

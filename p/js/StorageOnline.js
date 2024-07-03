3c46e433de3b88cc713f7d4ae652ab74f1786420
claudioolmedo <109242493+claudioolmedo@users.noreply.github.com>
2024-07-03 13:49:31 -0400

    Save in firebase if is null

--- p/js/StorageOnline.js
+++ p/js/StorageOnline.js
@@ -3,11 +3,14 @@ export function sendDataToStorageOnline(data, projectId) {
     console.log('Data received by StorageOnline:', data);
     console.log('Project ID received: in StorageOnline', projectId); // Log the received projectId
 
+    // Replace null or undefined values with an empty string
+    const sanitizedData = JSON.parse(JSON.stringify(data, (key, value) => value ?? ''));
+
     // Log the data before saving to Firebase
-    console.log('Data to be saved to Firebase:', JSON.stringify(data, null, 2));
+    console.log('Data to be saved to Firebase:', JSON.stringify(sanitizedData, null, 2));
 
     const path = `projects/${projectId}/state`; // Define the path where you want to save the data
-    saveData(path, data)
+    saveData(path, sanitizedData)
         .then(() => {
             console.log('Data successfully saved to Firebase from StorageOnline.');
         })

8434a81588240139c66944c9e007640559e2e789
claudioolmedo <109242493+claudioolmedo@users.noreply.github.com>
2024-07-02 20:31:44 -0400

    Ad consult from database.

--- p/js/StorageOnlineLoad.js
+++ p/js/StorageOnlineLoad.js
@@ -1,5 +1,24 @@
+import { ref, get } from './config/firebase.js';
 
 export function sendIDToStorageOnlineLoad(projectId) {
     console.log('Project ID received: in StorageOnline LOAD', projectId); // Log the received projectId
 
+    // Define the path to the project data
+    const path = `projects/${projectId}/state`;
+
+    // Create a reference to the database path
+    const projectRef = ref(firebaseDB, path);
+
+    // Get the data from the database
+    get(projectRef)
+        .then((snapshot) => {
+            if (snapshot.exists()) {
+                console.log('Project data:', snapshot.val()); // Log the project data
+            } else {
+                console.log('No data available for this project ID.');
+            }
+        })
+        .catch((error) => {
+            console.error('Error getting project data:', error);
+        });
 }
